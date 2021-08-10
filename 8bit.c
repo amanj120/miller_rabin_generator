e52a53038864161d1ba5b4dd926de4e21c0bd782
= <amanj120@gmail.com>
2021-08-09 22:03:18 -0400

    why does 17c7 ^ 0002 mod 8015 fail

--- 8bit.c
+++ 8bit.c
@@ -1,16 +1,19 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // 8 bit so it works on AVR
-typedef unsigned char byte;
+#define DEBUG 1
+
+typedef uint8_t byte;
 
 byte *new (byte size) { return calloc((1 << size), sizeof(byte)); }
 
-byte primes[53] = {2,	3,	 5,	  7,   11,	13,	 17,  23,  29,	31,	 37,
-				   41,	43,	 47,  53,  59,	61,	 67,  71,  73,	79,	 83,
-				   89,	97,	 101, 103, 107, 109, 113, 127, 131, 137, 139,
-				   149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197,
-				   199, 211, 223, 227, 229, 233, 239, 241, 251};
+byte primes[54] = {2,	3,	 5,	  7,   11,	13,	 17,  19,  23,	29,	 31,
+				   37,	41,	 43,  47,  53,	59,	 61,  67,  71,	73,	 79,
+				   83,	89,	 97,  101, 103, 107, 109, 113, 127, 131, 137,
+				   139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+				   197, 199, 211, 223, 227, 229, 233, 239, 241, 251};
 
 void clear(byte *src, byte size) {
 	for (int i = 0; i < (1 << size); i++) {
@@ -100,6 +103,7 @@ void add_const(byte *src, byte value, byte *dest, byte size) {
 
 // src1 -= src2, undefined behavior is src1 < src2
 void sub(byte *src1, byte *src2, byte *dest, byte size) {
+	// printf("in sub\n");
 	for (int i = 0; i < (1 << size); i++) {
 		if (src2[i] > src1[i]) {
 			// No out of bounds errors bc src1 > src2
@@ -127,6 +131,9 @@ void sub(byte *src1, byte *src2, byte *dest, byte size) {
 // size of dest must be twice that of src1 and src2
 // src1 and src2 must be the same size
 void mult(byte *src1, byte *src2, byte *dest, byte size) {
+	// if (DEBUG) {
+	// 	printf("in mult\n");
+	// }
 	clear(dest, size + 1);
 	int idx;
 	byte carry, temp;
@@ -153,12 +160,9 @@ void mult(byte *src1, byte *src2, byte *dest, byte size) {
 // size = log_2(size of n)
 // algorithm: binary search for q
 void mod(byte *a, byte *n, byte *dest, byte size) {
-	// printf("\ncalculating ");
-	// print(a, size + 1);
-	// printf(" mod ");
-	// print(n, size);
-	// printf("\n");
-
+	if (DEBUG) {
+		printf("in mod\n");
+	}
 	byte prev[1 << size];
 	byte zero[(1 << size)];
 	byte q_hi[1 << size];
@@ -184,19 +188,21 @@ void mod(byte *a, byte *n, byte *dest, byte size) {
 		rshift(q_mid, 1, size);
 		mult(q_mid, n, prod, size);
 
-		// printf("lo: ");
-		// print(q_lo, size);
-		// printf(" high: ");
-		// print(q_hi, size);
-		// printf(" mid: ");
-		// print(q_mid, size);
-		// printf(" prod: ");
-		// print(prod, size + 1);
-		// printf(" n: ");
-		// print(n, size);
-		// printf(" a: ");
-		// print(a, size + 1);
-		// printf(" \n");
+		if (DEBUG) {
+			printf("low: ");
+			print(q_lo, size);
+			printf(" high: ");
+			print(q_hi, size);
+			printf(" mid: ");
+			print(q_mid, size);
+			printf(" a: ");
+			print(a, size + 1);
+			printf(" prod: ");
+			print(prod, size + 1);
+			printf(" n: ");
+			print(n, size);
+			printf("\n");
+		}
 
 		if (compare(prod, a, size + 1) < 0) {
 			copy(q_mid, q_lo, size);
@@ -212,19 +218,26 @@ void mod(byte *a, byte *n, byte *dest, byte size) {
 		sub(a, prod, temp, size + 1);
 		copy(temp, dest, size);
 	}
+
+	if (DEBUG) {
+		printf("answer: ");
+		print(dest, size);
+		printf("\n");
+	}
 }
 
 // compute a^d mod n and store in dest
 // a, d, and n are all 2^size bytes wide
 void admodn(byte *a, byte *d, byte *n, byte *dest, byte size) {
-	// printf("\t\tcalculating ");
-	// print(a, size);
-	// printf(" ^ ");
-	// print(d, size);
-	// printf(" mod ");
-	// print(n, size);
-	// printf(": \n");
-
+	if (DEBUG) {
+		printf("calculating ");
+		print(a, size);
+		printf(" ^ ");
+		print(d, size);
+		printf(" mod ");
+		print(n, size);
+		printf("\n");
+	}
 	byte cur_a[1 << size];
 	byte prod[1 << size];
 	byte temp[1 << (size + 1)];
@@ -235,35 +248,23 @@ void admodn(byte *a, byte *d, byte *n, byte *dest, byte size) {
 
 	for (int i = 0; i < 1 << (size); i++) {
 		for (int j = 0; j < 8; j++) {
-
-			// printf("cur a: ");
-			// print(cur_a, size);
-			// printf("\nexp: %d value: %d\n", (i * 8 + j), (d[i] >> j) & 1);
-
 			// if exponent set, prod = (prod * cur_a) % n
 			if ((d[i] >> j) & 1 == 1) {
 				clear(temp, size);
 				mult(cur_a, prod, temp, size);
 				mod(temp, n, prod, size);
-				// printf("\tcur_prod: ");
-				// print(prod, size);
-				// printf("\n");
 			}
 			// regardless of whether exponent set:
-			// cur_a = (cur_a * cur_a) % n
-			// printf(" here ");
 			clear(temp, size);
 			mult(cur_a, cur_a, temp, size);
 			mod(temp, n, cur_a, size);
 		}
 	}
-
 	copy(prod, dest, size);
 }
 
 // p is the int we are testing for primality
 int miller_rabin(byte *p, byte size) {
-	// generate a prime
 	byte pm1[1 << size];
 	byte one[1 << size];
 	byte d[1 << size];
@@ -277,18 +278,16 @@ int miller_rabin(byte *p, byte size) {
 	byte two[1 << size];
 	set(two, 2, size);
 
-	for (int k = 0; k < 53; k++) {
-		printf("checking witness: %d\n", primes[k]);
+	for (int k = 0; k < 54; k++) {
+		if (DEBUG) {
+			printf("checking witness: %d\n", primes[k]);
+		}
 
 		byte a[1 << size];
 		byte dest[1 << size];
 		set(a, primes[k], size);
 		admodn(a, d, p, dest, size);
 		copy(dest, a, size);
-
-		// printf("\t");
-		// print(a, size);
-		// printf("\n");
 
 		if (compare(a, one, size) == 0 || compare(a, pm1, size) == 0) {
 			continue;
@@ -298,11 +297,6 @@ int miller_rabin(byte *p, byte size) {
 		for (int s = 0; s < shift; s++) {
 			admodn(a, two, p, dest, size);
 			copy(dest, a, size);
-
-			// printf("\t");
-			// print(a, size);
-			// printf("\n");
-
 			if (compare(a, pm1, size) == 0) {
 				is_witness = 1;
 				break;
@@ -314,109 +308,37 @@ int miller_rabin(byte *p, byte size) {
 			return 0;
 		}
 	}
-
 	return 1;
 }
 
+// exhaustive test of the system for all numbers [256, 65535]
+void test() {
+	for (int n = 257; n < 65536; n += 2) {
+		int is_prime = 1;
+		for (int p = 0; p < 54; p++) {
+			if (n % primes[p] == 0) {
+				is_prime = 0;
+				break;
+			}
+		}
+		byte test[2] = {(n & 0xff), ((n >> 8) & 0xff)};
+
+		int idx = is_prime + miller_rabin(test, 1);
+
+		if (idx == 0) {
+			printf("%d is composite\n", n);
+		} else if (idx == 1) {
+			printf("prime: %d miller rabin: %d\n", is_prime, idx - is_prime);
+			break;
+		} else {
+			printf("%d is prime\n", n);
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
-
-	// byte x63d[2] = {0x3d, 0x06};
-	// byte x000c7a01[4] = {0x01, 0x7a, 0x0c, 0x00};
-	// byte x0001[2] = {0x00, 0x00};
-	// mod(x000c7a01, x63d, x0001, 1);
-	// print(x0001, 1);
-	// printf("\n");
-
-	// calculating 000c7a01 mod 063d -> this is messing up
-
-	// byte ft[2] = {0x2b, 0x00};
-	// byte pow[2] = {0x8f, 0x01};
-	// byte n[2] = {0x3d, 0x06};
-	// byte dest[2] = {0x00, 0x00};
-	// admodn(ft, pow, n, dest, 1);
-
-	// print(dest, 1);
-	// printf("\n");
-
-	// byte a[4] = {0xcd, 0xab, 0x89, 0x67};
-	// byte b[4] = {0x78, 0x56, 0x34, 0x12};
-
-	// byte *prod = calloc(8, sizeof(byte));
-	// mult(&a[0], &b[0], prod, 2);
-
-	// for (int i = 7; i >= 0; i--) {
-	// 	printf("%02x", prod[i]);
-	// }
-	// printf("\n");
-
-	// printf("%d, %d\n", compare(a, b, 2), compare(b, a, 2));
-
-	// byte *aa = calloc(4, 1);
-	// add(a, b, aa, 2);
-	// for (int i = 3; i >= 0; i--) {
-	// 	printf("%02x", aa[i]);
-	// }
-	// printf("\n");
-
-	// sub(aa, b, aa, 2);
-	// for (int i = 3; i >= 0; i--) {
-	// 	printf("%02x", aa[i]);
-	// }
-	// printf("\n");
-
-	// sub(aa, b, aa, 2);
-	// for (int i = 3; i >= 0; i--) {
-	// 	printf("%02x", aa[i]);
-	// }
-	// printf("\n");
-
-	// sub(aa, b, aa, 2);
-	// for (int i = 3; i >= 0; i--) {
-	// 	printf("%02x", aa[i]);
-	// }
-	// printf("\n");
-
-	// rshift(aa, 3, 2);
-	// for (int i = 3; i >= 0; i--) {
-	// 	printf("%02x", aa[i]);
-	// }
-	// printf("\n"); // 00466926970ec559
-
-	// mult(aa, aa, prod, 2);
-	// for (int i = 7; i >= 0; i--) {
-	// 	printf("%02x", prod[i]);
-	// }
-	// printf("\n"); // so far so good:
-	// // 00466926970ec559
-
-	// byte *modulo = new (2);
-	// mod(prod, b, modulo, 2);
-	// for (int i = 3; i >= 0; i--) {
-	// 	printf("%02x", modulo[i]);
-	// }
-	// printf("\n");
-	// // should be: 0e962fc9
-
-	// free(prod);
-	// free(aa);
-	// free(modulo);
-
-	// byte base[2] = {0x00, 0x00};
-	// byte n[2] = {0x23, 0x02};
-	// byte exp[2] = {0x22, 0x02};
-	// byte dest[2] = {0x00, 0x00};
-
-	// for (int i = 0; i < 53; i++) {
-	// 	set(base, primes[i], 1);
-	// 	admodn(base, exp, n, dest, 1);
-	// 	print(dest, 1);
-	// 	printf("\n");
-	// }
-
-	// printf("%d, %d\n", find_lsb_set(0xf0), find_lsb_set(0x80));
-
-	byte test[2] = {0x3d, 0x06};
-	printf("answer: %d\n", miller_rabin(test, 1));
-
-	// return 0;
+	byte p[2] = {0x15, 0x80};
+	int x = miller_rabin(p, 1);
+	printf("miller rabin 0x8015: %d\n", x);
+	// test();
 }
