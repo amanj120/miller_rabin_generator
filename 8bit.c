5725790c02c1725923eddfef697edbc5814ea943
= <amanj120@gmail.com>
2021-08-11 18:32:58 -0400

    any size now works

--- 8bit.c
+++ 8bit.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 #include <sys/time.h>
+#include <time.h>
 
 // 8 bit so it works on AVR
 typedef unsigned char byte;
 
-byte primes[54] = {2,	3,	 5,	  7,   11,	13,	 17,  19,  23,	29,	 31,
+#define NP 53
+
+byte primes[NP] = {2,	3,	 5,	  7,   11,	13,	 17,  19,  23,	29,	 31,
 				   37,	41,	 43,  47,  53,	59,	 61,  67,  71,	73,	 79,
 				   83,	89,	 97,  101, 103, 107, 109, 113, 127, 131, 137,
 				   139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
-				   197, 199, 211, 223, 227, 229, 233, 239, 241, 251};
+				   197, 199, 211, 223, 227, 229, 233, 239, 241};
 
 // size = size of src in bytes
 void clear(byte *src, int size) {
@@ -148,7 +151,7 @@ void sub(byte *src1, byte *src2, byte *dest, int size) {
 // size of dest must be twice that of src1 and src2
 // src1 and src2 must be the same size
 void mult(byte *src1, byte *src2, byte *dest, int size) {
-	clear(dest, size + 1);
+	clear(dest, size << 1);
 	int idx;
 	byte carry, temp;
 	for (int i = 0; i < size; i++) {
@@ -172,14 +175,14 @@ void mult(byte *src1, byte *src2, byte *dest, int size) {
 
 void mod(byte *a, byte *n, byte *dest, int size) {
 	byte minus[size << 1];
-	clear(minus, size + 1);
+	clear(minus, size << 1);
 	copy(n, &minus[size], size); // minus = n << (1 << size);
 
 	for (int i = 0; i <= (size << 3); i++) {
-		if (compare(minus, a, size + 1) < 0) {
-			sub(a, minus, a, size + 1); // a -= (n << i)
+		if (compare(minus, a, size << 1) < 0) {
+			sub(a, minus, a, size << 1); // a -= (n << i)
 		}
-		rshift(minus, 1, size + 1);
+		rshift(minus, 1, size << 1);
 	}
 	copy(a, dest, size);
 }
@@ -187,7 +190,7 @@ void mod(byte *a, byte *n, byte *dest, int size) {
 // compute a^2 mod n and store in dest
 void asqmodn(byte *a, byte *n, byte *dest, int size) {
 	byte temp[size << 1];
-	clear(temp, size + 1);
+	clear(temp, size << 1);
 	mult(a, a, temp, size);
 	mod(temp, n, dest, size);
 }
@@ -233,11 +236,9 @@ int miller_rabin(byte *p, int size) {
 	byte shift = find_lsb_set(lsb);
 	rshift(d, shift, size);
 
-	for (int k = 0; k < 54; k++) {
+	for (int k = 0; k < NP; k++) {
 		set(a, primes[k], size);
 		admodn(a, d, p, a, size);
-
-		printf("Testing witness: %d\n", primes[k]);
 
 		p[0] &= 0xfe;
 		eqpm1 = compare(a, p, size);
@@ -275,7 +276,7 @@ void test() {
 	// for valgrind, run between 0x30001 and 0x30401
 	// for (long n = 0x30001; n < 0x30401; n += 2) {
 	// about ~0.5 million values: 0x101 - 0x100001
-	for (long n = 0x101; n < 0x40001; n += 2) {
+	for (long n = 0x101; n < 0x100001; n += 2) {
 		if ((n & 0xff) == 0x1) { // we can't handle these cases
 			printf("skipping %ld \n", n);
 			continue;
@@ -293,18 +294,18 @@ void test() {
 		int idx = is_prime + miller_rabin(test, 4);
 
 		if (idx == 0) {
-			printf("%ld is composite\n", n);
+			// printf("%ld is composite\n", n);
 		} else if (idx == 1) {
 			printf("%ld: prime: %d miller rabin: %d\n", n, is_prime,
 				   idx - is_prime);
 			break;
 		} else {
-			printf("%ld is prime\n", n);
+			// printf("%ld is prime\n", n);
 		}
 	}
 }
 
-void find(int size, int stats) {
+void find(int size, int print_stuff) {
 	byte *test = calloc(size, 1);
 
 	int runs = 0;
@@ -315,48 +316,47 @@ void find(int size, int stats) {
 	while (1) {
 		runs++;
 		rand_int(test, size);
-		printf("Testing: ");
-		print(test, size);
-		printf("\n");
+		if (print_stuff) {
+			printf("Testing: ");
+			print(test, size);
+			printf("\n");
+		}
 		if (miller_rabin(test, size) == 1) {
 			break;
 		}
 	}
 	gettimeofday(&end, NULL);
 
+	printf("\nFound probable prime:\n");
 	print(test, size);
-	printf(" is probably prime!\n");
+	printf("\n");
 
-	long diff = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	long diff = (end.tv_sec * 1000000 + end.tv_usec) -
+				(start.tv_sec * 1000000 + start.tv_usec);
 
-	if (stats) {
-		printf("took %d runs and %ld us\n", runs, diff);
-		printf("%d %ld\n", runs, diff); // for easier parsing
-
+	if (print_stuff) {
+		printf("\ntook %d runs and %ld us\n", runs, diff);
 	}
 }
 
-void print_help() {
-	printf("Use this program to generate random probable primes using the miller-rabin algorithm:");
-	// generated because 1 : 4 ^ 54 chance that the number is not prime 
-	printf("\"probable\": less than a 1 in 300,000,000,000,000,000,000,000,000,000,000 (3.2 * 10^32) chance that the output is not prime)\n");
-	// (2 ^ 108) / (2,500,000 L/pool 1000g/1L * 1 mol/ 18 g * 6.022*10^23 molecules/mol) = 3.88
-	printf("to put that into perspective, that's about how many MOLECULES of water there are in 4 olympic sized pools combined.\n");
-	
-	// or '--size=<valid size value>' 
-	printf("\nuse the flag '-s=<valid size value>' to pass in the size of the prime you would like to generate \n");
-	printf("valid values are: \n");
-	printf("\t'1': 16 bit prime\n");
-	printf("\t'2': 32 bit prime\n");
-	printf("\t'3': 64 bit prime\n");
-	printf("\t'4': 128 bit prime\n");
-	printf("\t'5': 256 bit prime\n");
-	printf("\t'6': 512 bit prime\n");
-	printf("\t'7': 1024 bit prime\n");
-	printf("\t'8': 2048 bit prime\n");
-	printf("The default value is 3 (64 bit prime)");
-	// or '--stats'
-	printf("use the flag '-t' to print stats about the run\n");
+void help() {
+	const char *help_message =
+		"Use this program to generate random probable primes using the\n"
+		"Miller-Rabin algorithm. There is a less than 1 in 2^106\n"
+		"chance that the output is not prime. For reference, that's\n"
+		"how many molecules of water there are in an olympic sized\n"
+		"swimming pool.\n\nUse -s=<size> to pass in the size of the"
+		" prime to generate in bytes.\nThe default is 256.\n\nUse -p"
+		" to print stats and progress about the run.\n";
+	printf("\n%s\n", help_message);
+}
+
+void seed() {
+	struct timeval seed;
+	gettimeofday(&seed, NULL);
+	long s_val = seed.tv_sec * 1000000 + seed.tv_usec;
+	int s_val_int = s_val & (((long)1 << 32) - 1);
+	srand(s_val_int);
 }
 
 // stack usage -> about 20x the size of the prime, need to cut down
@@ -364,42 +364,22 @@ void print_help() {
 // cut down to 9x, getting better; 7 probably the sweet spot
 // cut down to 8x by removing dest in miller rabin
 int main(int argc, char *argv[]) {
+	// test();
 
-	test();
+	seed();
+	int size = 256;
+	int print_stuff = 0;
 
-	// struct timeval seed;
-	// gettimeofday(&seed, NULL);
-	// long s_val = seed.tv_sec * 1000000 + seed.tv_usec;
-	// int s_val_int =  s_val & (((long)1 << 32) - 1);
-	// srand(s_val_int);
+	for (int i = 1; i < argc; i++) {
+		if (strncmp(argv[i], "-p", 2) == 0) {
+			print_stuff = 1;
+		} else if (strncmp(argv[i], "-s=", 3) == 0) {
+			size = atoi(&argv[i][3]);
+		} else {
+			help();
+			return 0;
+		}
+	}
 
-	// int size = 16;
-	// int stats = 1;
-
-	// if (argc == 2) {
-	// 	if (argv[1][0] == '-' && argv[1][1] == 's' && argv[1][2] == '=') {
-	// 		size = (int)argv[1][3] - 48; // (char)48 = '0';
-	// 		stats = 0;
-	// 	} else if (argv[1][0] == '-' && argv[1][1] == 't') {
-	// 		size = 5;
-	// 		stats = 1;
-	// 	} 
-	// } else if (argc == 3) {
-	// 	if (argv[1][0] == '-' && argv[1][1] == 's' && argv[1][2] == '=') {
-	// 		size = (int)argv[1][3] - 48; // (char)48 = '0';
-	// 	}
-	// 	if (argv[1][0] == '-' && argv[1][1] == 't') {
-	// 		stats = 1;
-	// 	} 
-	// 	if (argv[2][0] == '-' && argv[2][1] == 's' && argv[2][2] == '=') {
-	// 		size = (int)argv[1][3] - 48; // (char)48 = '0';
-	// 	}
-	// 	if (argv[2][0] == '-' && argv[2][1] == 't') {
-	// 		stats = 1;
-	// 	} 
-	// } else if (argc >= 4) {
-	// 	printf("invalid arguments, continuing with '-s=5 -t'\n");
-	// }
-
-	// find(size, stats);
+	find(size, print_stuff);
 }
