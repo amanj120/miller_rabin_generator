810cdee44eae6f24962562845edf215f7334ddbf
= <amanj120@gmail.com>
2021-08-11 17:57:16 -0400

    Something broke

--- 8bit.c
+++ 8bit.c
@@ -12,36 +12,37 @@ byte primes[54] = {2,	3,	 5,	  7,   11,	13,	 17,  19,  23,	29,	 31,
 				   139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
 				   197, 199, 211, 223, 227, 229, 233, 239, 241, 251};
 
-void clear(byte *src, byte size) {
-	for (int i = 0; i < (1 << size); i++) {
+// size = size of src in bytes
+void clear(byte *src, int size) {
+	for (int i = 0; i < size; i++) {
 		src[i] = 0;
 	}
 }
 
-void print(byte *src, byte size) {
-	for (int i = (1 << size) - 1; i >= 0; i--) {
+void print(byte *src, int size) {
+	for (int i = size - 1; i >= 0; i--) {
 		printf("%02x", src[i]);
 	}
 }
 
 // set the first byte in dest to constant, clear the rest
-void set(byte *dest, byte constant, byte size) {
+void set(byte *dest, byte constant, int size) {
 	clear(dest, size);
 	dest[0] = constant;
 }
 
-void copy(byte *src, byte *dest, byte size) {
-	for (int i = 0; i < (1 << size); i++) {
+void copy(byte *src, byte *dest, int size) {
+	for (int i = 0; i < size; i++) {
 		dest[i] = src[i];
 	}
 }
 
-void rand_int(byte *dest, byte size) {
-	for (int i = 0; i < (1 << size); i++) {
+void rand_int(byte *dest, int size) {
+	for (int i = 0; i < size; i++) {
 		dest[i] = rand() & 0xff;
 	}
 	dest[0] |= 1;
-	dest[(1 << size) - 1] |= 0x80;
+	dest[size - 1] |= 0x80;
 	// set another random bit in the least significant byte
 	// because right shift only works for shift < 8;
 	if (dest[0] == 1) {
@@ -51,12 +52,12 @@ void rand_int(byte *dest, byte size) {
 
 // 50 % of the run time is taken up by rshift
 // shift must be less than 8
-void rshift(byte *src, byte shift, byte size) {
+void rshift(byte *src, byte shift, int size) {
 	byte lshift = (8 - shift);
-	for (int i = 0; i < (1 << size) - 1; i++) {
+	for (int i = 0; i < size - 1; i++) {
 		src[i] = src[i] >> shift | src[i + 1] << lshift;
 	}
-	src[(1 << size) - 1] >>= shift;
+	src[size - 1] >>= shift;
 }
 
 byte find_lsb_set(byte src) {
@@ -68,8 +69,8 @@ byte find_lsb_set(byte src) {
 }
 
 // return <0  if src1 < src2, > 0 if src1 > src2, else 0
-int compare(byte *src1, byte *src2, byte size) {
-	for (int i = (1 << size) - 1; i >= 0; i--) {
+int compare(byte *src1, byte *src2, int size) {
+	for (int i = size - 1; i >= 0; i--) {
 		int t = src1[i] - src2[i];
 		if (t != 0) {
 			return t;
@@ -79,9 +80,9 @@ int compare(byte *src1, byte *src2, byte size) {
 }
 
 // dest = src1 + src2, returns carry
-byte add(byte *src1, byte *src2, byte *dest, byte size) {
+byte add(byte *src1, byte *src2, byte *dest, int size) {
 	byte carry = 0;
-	for (int i = 0; i < (1 << size); i++) {
+	for (int i = 0; i < size; i++) {
 		byte t = src1[i] + src2[i] + carry;
 		if (carry == 1) {
 			carry = (t <= src1[i]) ? 1 : 0;
@@ -93,27 +94,27 @@ byte add(byte *src1, byte *src2, byte *dest, byte size) {
 	return carry;
 }
 
-void add_const(byte *src, byte value, byte *dest, byte size) {
-	for (int i = 0; i < (1 << size); i++) {
+void add_const(byte *src, byte value, byte *dest, int size) {
+	for (int i = 0; i < size; i++) {
 		byte t = src[i] + value;
 		value = (t < src[i]) ? 1 : 0;
 		dest[i] = t;
 	}
 }
 
-void avg(byte *src1, byte *src2, byte *dest, byte size) {
+void avg(byte *src1, byte *src2, byte *dest, int size) {
 	byte carry = add(src1, src2, dest, size);
 	rshift(dest, 1, size);
 	if (carry) {
-		dest[(1 << size) - 1] |= 0x8f; // set the msb
+		dest[size - 1] |= 0x8f; // set the msb
 	}
 }
 
-byte is_const(byte *src, byte constant, byte size) {
+byte is_const(byte *src, byte constant, int size) {
 	if (src[0] != constant) {
 		return 0;
 	}
-	for (int i = 1; i < (1 << size); i++) {
+	for (int i = 1; i < size; i++) {
 		if (src[i] != 0) {
 			return 0;
 		}
@@ -122,11 +123,11 @@ byte is_const(byte *src, byte constant, byte size) {
 }
 
 // dest = src1 - src2, undefined behavior is src1 < src2
-void sub(byte *src1, byte *src2, byte *dest, byte size) {
+void sub(byte *src1, byte *src2, byte *dest, int size) {
 	byte t = 0;
-	for (int i = 0; i < (1 << size); i++) {
+	for (int i = 0; i < size; i++) {
 		if (src2[i] > src1[i]) { // No out of bounds errors bc src1 > src2
-			for (int j = i + 1; j < (1 << size); j++) {
+			for (int j = i + 1; j < size; j++) {
 				if (src1[j] == 0) {
 					src1[j] = 255;
 				} else {
@@ -146,12 +147,12 @@ void sub(byte *src1, byte *src2, byte *dest, byte size) {
 // size = log_2(length of src1, src2)
 // size of dest must be twice that of src1 and src2
 // src1 and src2 must be the same size
-void mult(byte *src1, byte *src2, byte *dest, byte size) {
+void mult(byte *src1, byte *src2, byte *dest, int size) {
 	clear(dest, size + 1);
 	int idx;
 	byte carry, temp;
-	for (int i = 0; i < (1 << size); i++) {
-		for (int j = 0; j < (1 << size); j++) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
 			temp = dest[i + j];
 			unsigned int p = src1[i] * src2[j]; // TODO: make this 8 bit
 			carry = (byte)((p >> 8) & 0xff);
@@ -169,12 +170,12 @@ void mult(byte *src1, byte *src2, byte *dest, byte size) {
 	return;
 }
 
-void mod(byte *a, byte *n, byte *dest, byte size) {
-	byte minus[1 << (size + 1)];
+void mod(byte *a, byte *n, byte *dest, int size) {
+	byte minus[size << 1];
 	clear(minus, size + 1);
-	copy(n, &minus[(1 << size)], size); // minus = n << (1 << size);
+	copy(n, &minus[size], size); // minus = n << (1 << size);
 
-	for (int i = 0; i <= (1 << (size + 3)); i++) {
+	for (int i = 0; i <= (size << 3); i++) {
 		if (compare(minus, a, size + 1) < 0) {
 			sub(a, minus, a, size + 1); // a -= (n << i)
 		}
@@ -184,8 +185,8 @@ void mod(byte *a, byte *n, byte *dest, byte size) {
 }
 
 // compute a^2 mod n and store in dest
-void asqmodn(byte *a, byte *n, byte *dest, byte size) {
-	byte temp[1 << (size + 1)];
+void asqmodn(byte *a, byte *n, byte *dest, int size) {
+	byte temp[size << 1];
 	clear(temp, size + 1);
 	mult(a, a, temp, size);
 	mod(temp, n, dest, size);
@@ -193,20 +194,20 @@ void asqmodn(byte *a, byte *n, byte *dest, byte size) {
 
 // compute a^d mod n and store in dest
 // a, d, and n are all 2^size bytes wide
-void admodn(byte *a, byte *d, byte *n, byte *dest, byte size) {
-	byte cur_a[1 << size];
-	byte prod[1 << size];
-	byte temp[1 << (size + 1)];
+void admodn(byte *a, byte *d, byte *n, byte *dest, int size) {
+	byte cur_a[size];
+	byte prod[size];
+	byte temp[size << 1];
 
 	clear(cur_a, size);
 	clear(prod, size);
-	clear(temp, size + 1);
+	clear(temp, size << 1);
 
 	set(prod, 1, size);
 
 	copy(a, cur_a, size); // current = a
 
-	for (int i = 0; i < 1 << (size); i++) {
+	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < 8; j++) {
 			// if exponent set, prod = (prod * cur_a) % n
 			if ((d[i] >> j) & 1 == 1) {
@@ -222,10 +223,10 @@ void admodn(byte *a, byte *d, byte *n, byte *dest, byte size) {
 }
 
 // p is the int we are testing for primality
-int miller_rabin(byte *p, byte size) {
+int miller_rabin(byte *p, int size) {
 	int eqpm1;
-	byte a[1 << size];
-	byte d[1 << size];
+	byte a[size];
+	byte d[size];
 	copy(p, d, size);
 
 	byte lsb = (p[0] & 0xfe);
@@ -235,6 +236,8 @@ int miller_rabin(byte *p, byte size) {
 	for (int k = 0; k < 54; k++) {
 		set(a, primes[k], size);
 		admodn(a, d, p, a, size);
+
+		printf("Testing witness: %d\n", primes[k]);
 
 		p[0] &= 0xfe;
 		eqpm1 = compare(a, p, size);
@@ -287,22 +290,22 @@ void test() {
 		}
 		byte test[4] = {(n & 0xff), ((n >> 8) & 0xff), ((n >> 16) & 0xff),
 						((n >> 24) & 0xff)};
-		int idx = is_prime + miller_rabin(test, 2);
+		int idx = is_prime + miller_rabin(test, 4);
 
 		if (idx == 0) {
-			// printf("%ld is composite\r", n);
+			printf("%ld is composite\n", n);
 		} else if (idx == 1) {
 			printf("%ld: prime: %d miller rabin: %d\n", n, is_prime,
 				   idx - is_prime);
 			break;
 		} else {
-			// printf("%ld is prime    \r", n);
+			printf("%ld is prime\n", n);
 		}
 	}
 }
 
 void find(int size, int stats) {
-	byte *test = calloc((1 << size), 1);
+	byte *test = calloc(size, 1);
 
 	int runs = 0;
 
@@ -362,39 +365,41 @@ void print_help() {
 // cut down to 8x by removing dest in miller rabin
 int main(int argc, char *argv[]) {
 
-	struct timeval seed;
-	gettimeofday(&seed, NULL);
-	long s_val = seed.tv_sec * 1000000 + seed.tv_usec;
-	int s_val_int =  s_val & (((long)1 << 32) - 1);
-	srand(s_val_int);
+	test();
 
-	int size = 5;
-	int stats = 1;
+	// struct timeval seed;
+	// gettimeofday(&seed, NULL);
+	// long s_val = seed.tv_sec * 1000000 + seed.tv_usec;
+	// int s_val_int =  s_val & (((long)1 << 32) - 1);
+	// srand(s_val_int);
 
-	if (argc == 2) {
-		if (argv[1][0] == '-' && argv[1][1] == 's' && argv[1][2] == '=') {
-			size = (int)argv[1][3] - 48; // (char)48 = '0';
-			stats = 0;
-		} else if (argv[1][0] == '-' && argv[1][1] == 't') {
-			size = 5;
-			stats = 1;
-		} 
-	} else if (argc == 3) {
-		if (argv[1][0] == '-' && argv[1][1] == 's' && argv[1][2] == '=') {
-			size = (int)argv[1][3] - 48; // (char)48 = '0';
-		}
-		if (argv[1][0] == '-' && argv[1][1] == 't') {
-			stats = 1;
-		} 
-		if (argv[2][0] == '-' && argv[2][1] == 's' && argv[2][2] == '=') {
-			size = (int)argv[1][3] - 48; // (char)48 = '0';
-		}
-		if (argv[2][0] == '-' && argv[2][1] == 't') {
-			stats = 1;
-		} 
-	} else if (argc >= 4) {
-		printf("invalid arguments, continuing with '-s=5 -t'\n");
-	}
+	// int size = 16;
+	// int stats = 1;
 
-	find(size, stats);
+	// if (argc == 2) {
+	// 	if (argv[1][0] == '-' && argv[1][1] == 's' && argv[1][2] == '=') {
+	// 		size = (int)argv[1][3] - 48; // (char)48 = '0';
+	// 		stats = 0;
+	// 	} else if (argv[1][0] == '-' && argv[1][1] == 't') {
+	// 		size = 5;
+	// 		stats = 1;
+	// 	} 
+	// } else if (argc == 3) {
+	// 	if (argv[1][0] == '-' && argv[1][1] == 's' && argv[1][2] == '=') {
+	// 		size = (int)argv[1][3] - 48; // (char)48 = '0';
+	// 	}
+	// 	if (argv[1][0] == '-' && argv[1][1] == 't') {
+	// 		stats = 1;
+	// 	} 
+	// 	if (argv[2][0] == '-' && argv[2][1] == 's' && argv[2][2] == '=') {
+	// 		size = (int)argv[1][3] - 48; // (char)48 = '0';
+	// 	}
+	// 	if (argv[2][0] == '-' && argv[2][1] == 't') {
+	// 		stats = 1;
+	// 	} 
+	// } else if (argc >= 4) {
+	// 	printf("invalid arguments, continuing with '-s=5 -t'\n");
+	// }
+
+	// find(size, stats);
 }
