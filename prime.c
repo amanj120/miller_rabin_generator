14c5be6a4d7b4e1deb18db3796539296b0e18c50
= <amanj120@gmail.com>
2021-08-11 20:07:32 -0400

    pretty things up a bit

--- prime.c
+++ prime.c
@@ -5,44 +5,51 @@
 #include <sys/time.h>
 #include <time.h>
 
-// 8 bit so it works on AVR
 typedef uint8_t byte;
+typedef int16_t word;
 
 #define NP 53
 
-byte primes[NP] = {2,	3,	 5,	  7,   11,	13,	 17,  19,  23,	29,	 31,
-				   37,	41,	 43,  47,  53,	59,	 61,  67,  71,	73,	 79,
-				   83,	89,	 97,  101, 103, 107, 109, 113, 127, 131, 137,
-				   139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
-				   197, 199, 211, 223, 227, 229, 233, 239, 241};
+static const byte primes[NP] = {
+	2,	 3,	  5,   7,	11,	 13,  17,  19,	23,	 29,  31,  37,	41,	 43,
+	47,	 53,  59,  61,	67,	 71,  73,  79,	83,	 89,  97,  101, 103, 107,
+	109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181,
+	191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241};
+
+static const char *help_message =
+	"\nThis program uses the Miller-Rabin algorithm to generate random "
+	"probable primes.\nThere is about a 1 in 2^106 chance that the output "
+	"is not prime.\nThat's how many molecules of water there are in an "
+	"Olympic sized swimming pool.\n\n\t-s <size> to set the size of the "
+	"generated prime in bits\n\n\t-d to print debug info\n\n";
 
 // size = size of src in bytes
-void clear(byte *src, int16_t size) {
-	for (int16_t i = 0; i < size; i++) {
+void clear(byte *src, word size) {
+	for (word i = 0; i < size; i++) {
 		src[i] = 0;
 	}
 }
 
-void print(byte *src, int16_t size) {
-	for (int16_t i = size - 1; i >= 0; i--) {
+void print(byte *src, word size) {
+	for (word i = size - 1; i >= 0; i--) {
 		printf("%02x", src[i]);
 	}
 }
 
 // set the first byte in dest to constant, clear the rest
-void set(byte *dest, byte constant, int16_t size) {
+void set(byte *dest, byte constant, word size) {
 	clear(dest, size);
 	dest[0] = constant;
 }
 
-void copy(byte *src, byte *dest, int16_t size) {
-	for (int16_t i = 0; i < size; i++) {
+void copy(byte *src, byte *dest, word size) {
+	for (word i = 0; i < size; i++) {
 		dest[i] = src[i];
 	}
 }
 
-void rand_int(byte *dest, int16_t size) {
-	for (int16_t i = 0; i < size; i++) {
+void rand_int(byte *dest, word size) {
+	for (word i = 0; i < size; i++) {
 		dest[i] = rand() & 0xff;
 	}
 	dest[0] |= 1;
@@ -56,9 +63,9 @@ void rand_int(byte *dest, int16_t size) {
 
 // 50 % of the run time is taken up by rshift
 // shift must be less than 8
-void rshift(byte *src, byte shift, int16_t size) {
+void rshift(byte *src, byte shift, word size) {
 	byte lshift = (8 - shift);
-	for (int16_t i = 0; i < size - 1; i++) {
+	for (word i = 0; i < size - 1; i++) {
 		src[i] = src[i] >> shift | src[i + 1] << lshift;
 	}
 	src[size - 1] >>= shift;
@@ -73,9 +80,9 @@ byte find_lsb_set(byte src) {
 }
 
 // return <0  if src1 < src2, > 0 if src1 > src2, else 0
-int16_t compare(byte *src1, byte *src2, int16_t size) {
-	for (int16_t i = size - 1; i >= 0; i--) {
-		int16_t t = src1[i] - src2[i];
+word compare(byte *src1, byte *src2, word size) {
+	for (word i = size - 1; i >= 0; i--) {
+		word t = src1[i] - src2[i];
 		if (t != 0) {
 			return t;
 		}
@@ -84,9 +91,9 @@ int16_t compare(byte *src1, byte *src2, int16_t size) {
 }
 
 // dest = src1 + src2, returns carry
-byte add(byte *src1, byte *src2, byte *dest, int16_t size) {
+byte add(byte *src1, byte *src2, byte *dest, word size) {
 	byte carry = 0;
-	for (int16_t i = 0; i < size; i++) {
+	for (word i = 0; i < size; i++) {
 		byte t = src1[i] + src2[i] + carry;
 		if (carry == 1) {
 			carry = (t <= src1[i]) ? 1 : 0;
@@ -98,15 +105,15 @@ byte add(byte *src1, byte *src2, byte *dest, int16_t size) {
 	return carry;
 }
 
-void add_const(byte *src, byte value, byte *dest, int16_t size) {
-	for (int16_t i = 0; i < size; i++) {
+void add_const(byte *src, byte value, byte *dest, word size) {
+	for (word i = 0; i < size; i++) {
 		byte t = src[i] + value;
 		value = (t < src[i]) ? 1 : 0;
 		dest[i] = t;
 	}
 }
 
-void avg(byte *src1, byte *src2, byte *dest, int16_t size) {
+void avg(byte *src1, byte *src2, byte *dest, word size) {
 	byte carry = add(src1, src2, dest, size);
 	rshift(dest, 1, size);
 	if (carry) {
@@ -114,11 +121,11 @@ void avg(byte *src1, byte *src2, byte *dest, int16_t size) {
 	}
 }
 
-byte is_const(byte *src, byte constant, int16_t size) {
+byte is_const(byte *src, byte constant, word size) {
 	if (src[0] != constant) {
 		return 0;
 	}
-	for (int16_t i = 1; i < size; i++) {
+	for (word i = 1; i < size; i++) {
 		if (src[i] != 0) {
 			return 0;
 		}
@@ -127,11 +134,11 @@ byte is_const(byte *src, byte constant, int16_t size) {
 }
 
 // dest = src1 - src2, undefined behavior is src1 < src2
-void sub(byte *src1, byte *src2, byte *dest, int16_t size) {
+void sub(byte *src1, byte *src2, byte *dest, word size) {
 	byte t = 0;
-	for (int16_t i = 0; i < size; i++) {
+	for (word i = 0; i < size; i++) {
 		if (src2[i] > src1[i]) { // No out of bounds errors bc src1 > src2
-			for (int16_t j = i + 1; j < size; j++) {
+			for (word j = i + 1; j < size; j++) {
 				if (src1[j] == 0) {
 					src1[j] = 255;
 				} else {
@@ -150,14 +157,14 @@ void sub(byte *src1, byte *src2, byte *dest, int16_t size) {
 
 // size of dest must be twice that of src1 and src2
 // src1 and src2 must be the same size
-void mult(byte *src1, byte *src2, byte *dest, int16_t size) {
+void mult(byte *src1, byte *src2, byte *dest, word size) {
 	clear(dest, size << 1);
-	int16_t idx;
+	word idx;
 	byte carry, temp;
-	for (int16_t i = 0; i < size; i++) {
-		for (int16_t j = 0; j < size; j++) {
+	for (word i = 0; i < size; i++) {
+		for (word j = 0; j < size; j++) {
 			temp = dest[i + j];
-			int16_t p = src1[i] * src2[j];
+			word p = src1[i] * src2[j];
 			carry = (byte)((p >> 8) & 0xff);
 			idx = 1;
 			dest[i + j] += (byte)(p & 0xff);
@@ -173,12 +180,12 @@ void mult(byte *src1, byte *src2, byte *dest, int16_t size) {
 	return;
 }
 
-void mod(byte *a, byte *n, byte *dest, int16_t size) {
+void mod(byte *a, byte *n, byte *dest, word size) {
 	byte minus[size << 1];
 	clear(minus, size << 1);
 	copy(n, &minus[size], size); // minus = n << (1 << size);
 
-	for (int16_t i = 0; i <= (size << 3); i++) {
+	for (word i = 0; i <= (size << 3); i++) {
 		if (compare(minus, a, size << 1) < 0) {
 			sub(a, minus, a, size << 1); // a -= (n << i)
 		}
@@ -188,7 +195,7 @@ void mod(byte *a, byte *n, byte *dest, int16_t size) {
 }
 
 // compute a^2 mod n and store in dest
-void asqmodn(byte *a, byte *n, byte *dest, int16_t size) {
+void asqmodn(byte *a, byte *n, byte *dest, word size) {
 	byte temp[size << 1];
 	clear(temp, size << 1);
 	mult(a, a, temp, size);
@@ -197,7 +204,7 @@ void asqmodn(byte *a, byte *n, byte *dest, int16_t size) {
 
 // compute a^d mod n and store in dest
 // a, d, and n are all 2^size bytes wide
-void admodn(byte *a, byte *d, byte *n, byte *dest, int16_t size) {
+void admodn(byte *a, byte *d, byte *n, byte *dest, word size) {
 	byte cur_a[size];
 	byte prod[size];
 	byte temp[size << 1];
@@ -210,8 +217,8 @@ void admodn(byte *a, byte *d, byte *n, byte *dest, int16_t size) {
 	copy(a, cur_a, size);
 
 	// binary exponentiation
-	for (int16_t i = 0; i < size; i++) {
-		for (int16_t j = 0; j < 8; j++) {
+	for (word i = 0; i < size; i++) {
+		for (word j = 0; j < 8; j++) {
 			if ((d[i] >> j) & 1 == 1) {
 				mult(cur_a, prod, temp, size);
 				mod(temp, n, prod, size);
@@ -223,9 +230,9 @@ void admodn(byte *a, byte *d, byte *n, byte *dest, int16_t size) {
 	copy(prod, dest, size);
 }
 
-// p is the int16_t we are testing for primality
-int16_t miller_rabin(byte *p, int16_t size) {
-	int16_t eqpm1;
+// p is the word we are testing for primality
+word miller_rabin(byte *p, word size) {
+	word eqpm1;
 	byte a[size];
 	byte d[size];
 	copy(p, d, size);
@@ -234,7 +241,7 @@ int16_t miller_rabin(byte *p, int16_t size) {
 	byte shift = find_lsb_set(lsb);
 	rshift(d, shift, size);
 
-	for (int16_t k = 0; k < NP; k++) {
+	for (word k = 0; k < NP; k++) {
 		set(a, primes[k], size);
 		admodn(a, d, p, a, size);
 
@@ -246,12 +253,12 @@ int16_t miller_rabin(byte *p, int16_t size) {
 			continue;
 		}
 
-		int16_t is_witness = 0;
-		for (int16_t s = 0; s < shift; s++) {
+		word is_witness = 0;
+		for (word s = 0; s < shift; s++) {
 			asqmodn(a, p, a, size);
 
 			p[0] &= 0xfe;
-			int16_t eqpm1 = compare(a, p, size);
+			word eqpm1 = compare(a, p, size);
 			p[0] |= 1;
 
 			if (eqpm1 == 0) {
@@ -269,9 +276,9 @@ int16_t miller_rabin(byte *p, int16_t size) {
 	return 1;
 }
 
-void find(int16_t size, int16_t print_stuff) {
+void find(word size, word print_stuff) {
 	byte *test = calloc(size, 1);
-	int16_t runs = 0;
+	word runs = 0;
 	struct timeval start, end;
 
 	gettimeofday(&start, NULL);
@@ -300,15 +307,8 @@ void find(int16_t size, int16_t print_stuff) {
 	}
 }
 
-int16_t help() {
-	const char *help_message =
-		"This program uses the Miller-Rabin algorithm to generate random "
-		"probable primes.\nThere is a less than 1 in 2^106 chance that "
-		"the output is not prime.\nThat's how many molecules of water "
-		"there are in an Olympic sized swimming pool.\n\n\t-s <size> "
-		"to set the size of the generated prime in bits"
-		"\n\n\t-d to print debug info";
-	printf("\n%s\n\n", help_message);
+word help() {
+	printf("%s", help_message);
 	return 0;
 }
 
@@ -316,8 +316,8 @@ void seed() {
 	struct timeval seed;
 	gettimeofday(&seed, NULL);
 	long s_val = seed.tv_sec * 1000000 + seed.tv_usec;
-	int16_t s_val_int16_t = s_val & (((long)1 << 32) - 1);
-	srand(s_val_int16_t);
+	word s_val_word = s_val & (((long)1 << 32) - 1);
+	srand(s_val_word);
 }
 
 // somewhat exhaustive test of the system
@@ -331,8 +331,8 @@ void test() {
 			continue;
 		}
 
-		int16_t is_prime = 1;
-		for (int16_t d = 3; d * d <= n; d += 2) {
+		word is_prime = 1;
+		for (word d = 3; d * d <= n; d += 2) {
 			if (n % d == 0) {
 				is_prime = 0;
 				break;
@@ -340,7 +340,7 @@ void test() {
 		}
 		byte test[4] = {(n & 0xff), ((n >> 8) & 0xff), ((n >> 16) & 0xff),
 						((n >> 24) & 0xff)};
-		int16_t idx = is_prime + miller_rabin(test, 4);
+		word idx = is_prime + miller_rabin(test, 4);
 
 		if (idx == 0) {
 			// printf("%ld is composite\n", n);
@@ -355,14 +355,14 @@ void test() {
 }
 
 // total stack usage -> about 8x size of prime
-int16_t main(int16_t argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	// test();
 
 	seed();
-	int16_t size = 0;
-	int16_t print_stuff = 0;
+	word size = 0;
+	word print_stuff = 0;
 
-	for (int16_t i = 1; i < argc; i++) {
+	for (word i = 1; i < argc; i++) {
 		if (strncmp(argv[i], "-d", 2) == 0) {
 			print_stuff = 1;
 		} else if (strncmp(argv[i], "-s", 2) == 0) {
