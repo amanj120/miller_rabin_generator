ab859a90b89eba24634a693055dd209b7763ac0d
Aman Jain <amanj120@gmail.com>
2022-05-14 01:38:31 -0400

    got prime32 to work

--- prime32.c
+++ prime32.c
@@ -58,9 +58,6 @@ void rand_int(unit *dest, unit size) {
 	}
 	dest[0] |= 3; // we're pinning the 2nd least bit to be 1 so that the miller rabin test is easier to do
 	// dest[size - 1] |= 0x80000000; //pinning the msb to be 1
-	printf("in rand int: ");
-	print(dest, size);
-	printf("\n");
 }
 
 // 50 % of the run time is taken up by rshift
@@ -85,9 +82,9 @@ void rshift(unit *src, unit shift, unit size) {
 // }
 
 // return <0  if src1 < src2, > 0 if src1 > src2, else 0
-unit compare(unit *src1, unit *src2, unit size) {
-	for (sunit i = size - 1; i >= 0; i--) {
-		sunit t = src1[i] - src2[i];
+sunit compare(unit *src1, unit *src2, unit size) {
+	for (sunit i = ((sunit)size) - 1; i >= 0; i--) {
+		sunit t = ((sunit)src1[i]) - ((sunit)src2[i]);
 		if (t < 0) {
 			return -1;
 		} else if (t > 0) {
@@ -163,7 +160,7 @@ void mult(unit *src1, unit *src2, unit *dest, unit size) {
 	for (unit i = 0; i < size; i++) {
 		for (unit j = 0; j < size; j++) {
 			temp = dest[i + j];
-			uint64_t p = src1[i] * src2[j];
+			uint64_t p = ((uint64_t)src1[i]) * ((uint64_t)src2[j]);
 			carry = (unit)((p >> 32) & 0xffffffff);
 			idx = 1;
 			dest[i + j] += (unit)(p & 0xffffffff);
@@ -179,7 +176,7 @@ void mult(unit *src1, unit *src2, unit *dest, unit size) {
 	return;
 }
 
-// assume a is twice as big as n
+// assume a is twice the size as n
 // a = qn + r for some integer q and some r in [0, n); return r
 void mod(unit *a, unit *n, unit *dest, unit size) {
 	if (compare(a, n, size) < 0 && is_const(&a[size], 0, size)) {
@@ -191,7 +188,7 @@ void mod(unit *a, unit *n, unit *dest, unit size) {
 	clear(minus, bigsize);
 	copy(n, &minus[size], size); // minus = n << (1 << size);
 
-	for (unit i = 0; i <= (size << 5); i++) {
+	for (unit i = 0; i <= (size * 32); i++) {
 		if (compare(minus, a, bigsize) < 0) {
 			sub(a, minus, a, bigsize); // a -= (n << i)
 		}
@@ -211,13 +208,14 @@ void mod(unit *a, unit *n, unit *dest, unit size) {
 // compute a^d mod n and store in dest
 // a, d, and n are all 2^size units wide
 void admodn(unit *a, unit *d, unit *n, unit *dest, unit size) {
+	unit bigsize = size << 1;
 	unit cur_a[size];
 	unit prod[size];
-	unit temp[size << 1];
+	unit temp[bigsize];
 
 	clear(cur_a, size);
 	clear(prod, size);
-	clear(temp, size << 1);
+	clear(temp, bigsize);
 
 	set(prod, 1, size);
 	copy(a, cur_a, size);
@@ -240,9 +238,9 @@ void admodn(unit *a, unit *d, unit *n, unit *dest, unit size) {
 unit miller_rabin(unit *p, unit size) {
 	unit eqpm1;
 	unit a[size];
+
 	unit d[size];
 	copy(p, d, size);
-
 	rshift(d, 1, size); // because we know the 2nd least bit is a 1
 
 	for (unit k = 0; k < NP; k++) {
@@ -254,8 +252,8 @@ unit miller_rabin(unit *p, unit size) {
 		p[0] |= 1;
 
 		if (is_const(a, 1, size) != 1 && eqpm1 != 0) { // if a^d === 1 or -1 mod p, a is a witness; otherwise it is not
-			print(a, size);
-			printf(" no witness, a is 1 ? %d a is -1 ? %d\n", is_const(a, 1, size), eqpm1);
+			// print(a, size);
+			// printf(" no witness, a is 1 ? %d a is -1 ? %d\n", is_const(a, 1, size), eqpm1);
 			return 0;
 		}
 	}
@@ -296,7 +294,7 @@ void find(unit size, unit print_stuff) {
 	while (++runs) {
 		rand_int(test, size);
 		if (print_stuff) {
-			printf("Testing w size %d: ", size);
+			printf("Testing ");
 			print(test, size);
 			printf("\n");
 		}
@@ -318,7 +316,7 @@ void find(unit size, unit print_stuff) {
 				(start.tv_sec * 1000000 + start.tv_usec);
 
 	if (print_stuff) {
-		printf("\ntook %ld runs and %ld us\n", runs, diff);
+		printf("\ntook %d runs and %ld us\n", runs, diff);
 	}
 
 	free(test);
@@ -362,7 +360,7 @@ void test() {
 		if (idx == 0) {
 			// printf("%ld is composite\n", n);
 		} else if (idx == 1) {
-			printf("%ld: prime: %ld miller rabin: %ld\n", n, is_prime,
+			printf("%ld: prime: %d miller rabin: %d\n", n, is_prime,
 				   idx - is_prime);
 			break;
 		} else {
